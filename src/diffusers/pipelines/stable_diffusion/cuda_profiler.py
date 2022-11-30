5bf0dbd4cfecc03e9c67d66597d3effcbe2363ff
Tianlei Wu <tlwu@microsoft.com>
2022-11-30 12:46:02 -0800

    support windows

--- src/diffusers/pipelines/stable_diffusion/cuda_profiler.py
+++ src/diffusers/pipelines/stable_diffusion/cuda_profiler.py
@@ -1,16 +1,29 @@
 import ctypes
-
+import platform
 
 class CudaProfiler:
     def __init__(self):
-        self._cudart = ctypes.CDLL("libcudart.so")
+
+        system = platform.system()
+        self._cudart = None
+        if system == "Linux":
+            self._cudart = ctypes.cdll.LoadLibrary("libcudart.so")
+        elif system == "Darwin":
+            self._cudart = ctypes.cdll.LoadLibrary("libcudart.dylib")
+        elif system == "Windows":
+            self._cudart = ctypes.windll.LoadLibrary("cudart64_110.dll")
+        else:
+            raise Exception("Cannot identify system.")
+
 
     def start(self):
-        ret = self._cudart.cudaProfilerStart()
-        if ret != 0:
-            raise Exception("cudaProfilerStart() returned %d" % ret)
+        if self._cudart:
+            ret = self._cudart.cudaProfilerStart()
+            if ret != 0:
+                raise Exception("cudaProfilerStart() returned %d" % ret)
 
     def stop(self):
-        ret = self._cudart.cudaProfilerStop()
-        if ret != 0:
-            raise Exception("cudaProfilerStop() returned %d" % ret)
+        if self._cudart:
+            ret = self._cudart.cudaProfilerStop()
+            if ret != 0:
+                raise Exception("cudaProfilerStop() returned %d" % ret)
